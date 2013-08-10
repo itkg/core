3a4bed4f3d29ac77eaa9e695b5f369ab35cf52e8
Pascal DENIS <pascal.denis@businessdecision.com>
2013-08-11 00:59:43 +0200

    Add config mgmt

---
+++ src/Lemon/Config.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Lemon;
+
+/**
+ * Classe NotFoundException
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class Config
+{
+	protected $params;
+	protected $requiredParams = array();
+
+	public function getParams()
+	{
+		if(!is_array($this->params)) {
+			$this->params = array();
+		}
+		return $this->params;
+	}
+
+	public function setParams(array $params = array())
+	{
+		$this->params = $params;
+		$this->validateParams();
+	}
+
+	public function addParam($key, $value)
+	{
+		$this->params[$key] = $value;
+	}
+
+	public function mergeParams(array $params = array())
+	{
+		$this->setParams(array_merge($this->getParams(), $params));
+	}
+
+	public function validateParams()
+	{
+		foreach($requiredParams as $key) {
+			if(!isset($this->params[$key])) {
+				throw new \Lemon\Exception\ConfigException(
+					sprintf('Parameter %s is required for class %s', $key, get_class())
+				);
+			}
+		}
+	}
+}
---
+++ src/Lemon/Exception/ConfigException.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Lemon\Exception;
+
+/**
+ * Classe ConfigException
+ *
+ * @author Pascal DENIS <pascal.denis.75@gmail.com>
+ */
+class ConfigException extends \Exception
+{
+    
+}
