ec38a7e76db5337939a51a91e42e3c8dbfc20c83
Pascal DENIS <pascal.denis@businessdecision.com>
2014-07-10 18:31:26 +0200

    service register

--- src/Itkg/Core/Model/ServiceContainer.php
+++ src/Itkg/Core/Model/ServiceContainer.php
@@ -2,6 +2,7 @@
 
 namespace Itkg\Core\Model;
 
+use Itkg\Core\Provider\ServiceProviderInterface;
 use Pimple;
 
 /**
@@ -12,5 +13,24 @@ class ServiceContainer extends Pimple
     public function __construct(ConfigInterface $config)
     {
         $this['config'] = $config;
+    }
+
+    /**
+     * Registers a service provider.
+     *
+     * @param ServiceProviderInterface $provider A ServiceProviderInterface instance
+     * @param array                    $values   An array of values that customizes the provider
+     *
+     * @return static
+     */
+    public function register(ServiceProviderInterface $provider, array $values = array())
+    {
+        $provider->register($this);
+
+        foreach ($values as $key => $value) {
+            $this[$key] = $value;
+        }
+
+        return $this;
     }
 }
