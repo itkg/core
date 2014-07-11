0394e8e3c34505e551301da383d96b834f9526d0
Pascal DENIS <pascal.denis.75@gmail.com>
2014-07-11 21:12:27 +0200

    loader test

---
+++ tests/Itkg/Core/Command/Script/LoaderTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Itkg\Core\Command\Script;
+
+use Doctrine\DBAL\Configuration;
+use Doctrine\DBAL\DriverManager;
+
+class LoaderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testLoad()
+    {
+        $loader = $this->createLoader();
+        $loader->load(__DIR__.'/../../../../mock/ticket.php');
+        $this->assertEquals(1, count($loader->getQueries()));
+
+        $loader->load(__DIR__.'/../../../../mock/ticket.php');
+        $this->assertEquals(1, count($loader->getQueries())); /* Reset queries before load */
+    }
+
+    public function testAddQuery()
+    {
+        $query = 'A new SQL query';
+        $loader = $this->createLoader();
+        $loader->addQuery($query);
+        $this->assertEquals(array($query), $loader->getQueries());
+    }
+
+    public function testAddQueryBuilder()
+    {
+        $loader = $this->createLoader();
+        $qb = $loader->getQueryBuilder();
+        $qb
+            ->select('u.id', 'u.name')
+            ->from('users', 'u');
+        $loader->addQueryFromBuilder($qb);
+        $query = 'SELECT u.id, u.name FROM users u';
+        $this->assertEquals(array($query), $loader->getQueries());
+    }
+
+    public function testQueryBuilder()
+    {
+        $this->assertInstanceOf('Doctrine\DBAL\Query\QueryBuilder', $this->createLoader()->getQueryBuilder());
+    }
+
+    private function createLoader()
+    {
+        $params = array(
+            'dbname' => 'DBNAME',
+            'user'   => 'USER',
+            'password' => 'PWD',
+            'host' => '',
+            'driver' => 'oci8'
+        );
+
+        $config = new Configuration();
+        $connection = DriverManager::getConnection($params, $config);
+
+        return new Loader($connection);
+    }
+} 
