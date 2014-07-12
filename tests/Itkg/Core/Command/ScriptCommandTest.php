66be45339ef87a0f02355a0fddc045a1fed15f14
Pascal DENIS <pascal.denis.75@gmail.com>
2014-07-12 22:01:32 +0200

    test script command

--- tests/Itkg/Core/Command/ScriptCommandTest.php
+++ tests/Itkg/Core/Command/ScriptCommandTest.php
@@ -8,6 +8,8 @@ use Itkg\Core\Command\Script\Loader;
 use Itkg\Core\Command\Script\Migration\Factory;
 use Itkg\Core\Command\Script\Runner;
 use Itkg\Core\Command\Script\Setup;
+use Symfony\Component\Console\Application;
+use Symfony\Component\Console\Tester\CommandTester;
 
 class ScriptCommandTest extends \PHPUnit_Framework_TestCase
 {
@@ -17,6 +19,48 @@ class ScriptCommandTest extends \PHPUnit_Framework_TestCase
         $command = $this->createCommand();
 
         $this->assertEquals(array($script), $command->getScripts(__DIR__.'/../../../mock/script'));
+    }
+
+    /**
+     * @expectedException \RuntimeException
+     */
+    public function testWithNoScripts()
+    {
+        $command = $this->createCommand();
+
+        $application = new Application();
+        $application->add($command);
+
+        if(!file_exists( __DIR__.'/../../../mock/void')) {
+            mkdir( __DIR__.'/../../../mock/void');
+            mkdir( __DIR__.'/../../../mock/void/script');
+            mkdir( __DIR__.'/../../../mock/void/rollback');
+        }
+
+        $command = $application->find('itkg-core:script');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName(), 'source' => __DIR__.'/../../../mock/void'));
+    }
+
+    /**
+     * @expectedException \LogicException
+     */
+    public function testUncompleteScripts()
+    {
+        $command = $this->createCommand();
+
+        $application = new Application();
+        $application->add($command);
+
+        if(!file_exists( __DIR__.'/../../../mock/void')) {
+            mkdir( __DIR__.'/../../../mock/void');
+            mkdir( __DIR__.'/../../../mock/void/script');
+            mkdir( __DIR__.'/../../../mock/void/rollback');
+        }
+
+        $command = $application->find('itkg-core:script');
+        $commandTester = new CommandTester($command);
+        $commandTester->execute(array('command' => $command->getName(), 'source' => __DIR__.'/../../../mock/uncomplete'));
     }
 
     private function createCommand()
@@ -36,6 +80,7 @@ class ScriptCommandTest extends \PHPUnit_Framework_TestCase
         $runner = new Runner($connection);
         $factory = new Factory();
 
-        return new ScriptCommand('name', new Setup($runner, $loader, $factory));
+        return new ScriptCommand('itkg-core:script', new Setup($runner, $loader, $factory));
     }
+
 } 
---
+++ tests/mock/uncomplete/script/ticket.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pdenis
+ * Date: 12/07/14
+ * Time: 21:59
+ */ 
