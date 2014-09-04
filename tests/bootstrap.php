27de04859e097d9e23beb197a2a4815125cbb1c3
Mathieu Duplouy <mathieu.duplouy@businessdecision.com>
2014-09-04 17:44:20 +0200

    Fixed autoload file path for tests bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,7 @@
 
 define ('TEST_BASE_DIR', __DIR__);
 
-if (!file_exists($autoloadFile = __DIR__.'/../../../../vendor/autoload.php')) {
+if (!file_exists($autoloadFile = __DIR__.'/../vendor/autoload.php')) {
     throw new \LogicException('Could not find autoload.php in vendor/. Did you run "composer install --dev"?');
 }
 
